eab9bcdc3df4eab78daff23956005bf910cd8403
makubex2010 <67411136+makubex2010@users.noreply.github.com>
2021-11-16 02:18:24 +0800

    Update psnidbot.py

--- psnidbot.py
+++ psnidbot.py
@@ -8,7 +8,7 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
 updater = Updater(token="2132340913:AAGeFSdbISuDcCAZB3q42PXtFfojjB2j1O8")
-MONGODB_CLIENT = 'mongodb+srv://makubex2010:306578@cluster0.kjrdp.mongodb.net/myFirstDatabase'
+MONGODB_CLIENT = 'mongodb+srv://makubex2010:306578@cluster0.kjrdp.mongodb.net/psnid?retryWrites=true&w=majority'
 DB_NAME = 'psnid'
 COLLECTION_NAME = 'PSNID'
 

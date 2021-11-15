e38f98ce6965670e11f4b8e1b6a905cf1cd6b8ec
makubex2010 <67411136+makubex2010@users.noreply.github.com>
2021-11-16 04:26:58 +0800

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python3
 
-import pymysql
+import pymongo
 
 def connectDB(cmd):
-        db = pymysql.connect("s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "3306", "u0oumepy212w7hrd", "lmhzodresmb5a1dr", "wagjozo03g4tc27f", charset='utf8' )
+        db = pymongo.connect("cluster0.kjrdp.mongodb.net", "makubex2010", "306578", "db", charset='utf8' )
         cursor = db.cursor()
         cursor.execute(cmd)
         data = cursor.fetchone()

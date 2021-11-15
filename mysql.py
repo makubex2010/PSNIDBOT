707ad6a0798c994ed8d8f126cea5032cfbfe3305
makubex2010 <67411136+makubex2010@users.noreply.github.com>
2021-11-16 05:40:58 +0800

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -3,7 +3,7 @@
 import pymysql
 
 def connectDB(cmd):
-        db = pymysql.connect(s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com, wagjozo03g4tc27f, u0oumepy212w7hrd, lmhzodresmb5a1dr)
+        db = pymysql.connect("s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "wagjozo03g4tc27f", "u0oumepy212w7hrd", "lmhzodresmb5a1dr")
         cursor = db.cursor()
         cursor.execute(cmd)
         data = cursor.fetchone()

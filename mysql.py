8f4348a6942346a2d71716116f3358d6b494a0dd
makubex2010 <67411136+makubex2010@users.noreply.github.com>
2021-11-15 22:16:28 +0800

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -11,25 +11,25 @@ def connectDB(cmd):
         db.close()
         return data
 
-def inserttodb(userid, liveid, username = None):
-        cmd = "INSERT INTO xboxliveid(userid,liveid,username)VALUES(%d,'%s','%s')" \
-        % (userid, pymysql.escape_string(liveid), pymysql.escape_string(username))
+def inserttodb(userid, psnid, username = None):
+        cmd = "INSERT INTO PlayStationNetworkID(userid,liveid,username)VALUES(%d,'%s','%s')" \
+        % (userid, pymysql.escape_string(psnid), pymysql.escape_string(username))
         connectDB(cmd)
 
 def changeondb(userid, liveid, username = None):
-        cmd = "UPDATE xboxliveid set liveid='%s' WHERE userid=%d" % (pymysql.escape_string(liveid), userid)
-        cmd2 = "UPDATE xboxliveid set username='%s' WHERE userid=%d" % (pymysql.escape_string(username), userid)
+        cmd = "UPDATE PlayStationNetworkID set psnid='%s' WHERE userid=%d" % (pymysql.escape_string(psnid), userid)
+        cmd2 = "UPDATE PlayStationNetworkID set username='%s' WHERE userid=%d" % (pymysql.escape_string(username), userid)
         connectDB(cmd)
         connectDB(cmd2)
 
 def searchname(username):
-        cmd = "SELECT liveid FROM xboxliveid WHERE username='%s'" % username
+        cmd = "SELECT psnid FROM PlayStationNetworkID WHERE username='%s'" % username
         data = connectDB(cmd)
         data = ''.join(data)
         return data
 
 def searchindb(userid):
-        cmd = "SELECT liveid FROM xboxliveid WHERE userid=%d" % userid
+        cmd = "SELECT psnid FROM PlayStationNetworkID WHERE userid=%d" % userid
         data = connectDB(cmd)
         try:
                 data = ''.join(data)

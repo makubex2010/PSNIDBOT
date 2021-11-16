927393a01f609c925607f933674a19577b1d95a6
makubex2010 <67411136+makubex2010@users.noreply.github.com>
2021-11-16 11:43:37 +0800

    Update psnidbot.py

--- psnidbot.py
+++ psnidbot.py
@@ -11,6 +11,7 @@ from telegram.ext import CommandHandler
 admins = []
 rolllist = []
 rollid = 100
+args = pass_args
 updater = Updater(token="2132340913:AAGeFSdbISuDcCAZB3q42PXtFfojjB2j1O8")
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)

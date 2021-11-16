37010b396e2569f767fb936180ab330511a4118b
makubex2010 <67411136+makubex2010@users.noreply.github.com>
2021-11-16 21:50:06 +0800

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -3,13 +3,7 @@
 import pymysql
 
 def connectDB(cmd): 
-         config = {
-     "host":"us-cdbr-east-04.cleardb.com",
-     "user":"be3f72595e2b4f",
-     "password":"1b092851",
-     "database":"heroku_aa93acde8a5d2ff"
-  }
-        db = pymysql.connect(**config)
+        db = pymysql.connect("host":"us-cdbr-east-04.cleardb.com", "user":"be3f72595e2b4f", "password":"1b092851", "database":"heroku_aa93acde8a5d2ff" )
         cursor = db.cursor()
         cursor.execute(cmd)
         results = cursor.fetchall()

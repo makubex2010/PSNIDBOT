77bf071f1123c5b3516c86224404413fef5458e8
makubex2010 <67411136+makubex2010@users.noreply.github.com>
2021-11-16 12:19:29 +0800

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -6,7 +6,7 @@ def connectDB(cmd):
         db = "mysql://be3f72595e2b4f:1b092851@us-cdbr-east-04.cleardb.com/heroku_aa93acde8a5d2ff?reconnect=true"
         cursor = db.cursor()
         cursor.execute(cmd)
-        data = cursor.fetchone()
+        results = cursor.fetchall()
         db.commit()
         db.close()
         return data

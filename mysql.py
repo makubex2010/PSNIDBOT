a3e2e86df359c57c3e62f0c3e8363105e22c7a5f
makubex2010 <67411136+makubex2010@users.noreply.github.com>
2021-11-22 13:57:50 +0800

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -3,7 +3,7 @@
 import pymysql
 
 def connectDB(cmd): 
-        db = pymysql.connect("tb9ia3h1pgevwb1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "rfrwe8ksy97efx1k", "epawk56f10zxjf9b", "oyszn9xf2whgmb2j", charset='utf8' )
+        db = pymysql.connect("bb2d60ffa470c9", "9027fcbf", "us-cdbr-east-04.cleardb.com", "heroku_ec10c9bdacf1022", charset='utf8' )
         cursor = db.cursor()
         cursor.execute(cmd)
         data = cursor.fetchone()

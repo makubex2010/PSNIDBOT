87f6bb39504cfa76e1788669f8cc6afb7744d7fc
makubex2010 <67411136+makubex2010@users.noreply.github.com>
2021-11-23 02:22:45 +0800

    Update psnidbot.py

--- psnidbot.py
+++ psnidbot.py
@@ -58,11 +58,11 @@ def searchid(bot, update, args):
                 else:
                         user = update.message.reply_to_message.from_user
                         psnid = mysql.searchindb(user.id)
-                whose = '他的PSNID是: 喔!'
+                whose = '他的PSNID是: '
         except:
                 user = update.message.from_user
                 psnid = mysql.searchindb(user.id)
-                whose = '你的PSNID是: 喔!'
+                whose = '你的PSNID是: '
         if(psnid == -1):
                 psnid = '-1'
         msgid = update.message.message_id

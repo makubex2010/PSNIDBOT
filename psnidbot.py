d0e00fa846216a5ba213d1df58065d8fb4794096
makubex2010 <67411136+makubex2010@users.noreply.github.com>
2021-11-24 04:44:11 +0800

    Update psnidbot.py

--- psnidbot.py
+++ psnidbot.py
@@ -59,17 +59,15 @@ def searchid(bot, update, args):
                 else:
                         user = update.message.reply_to_message.from_user
                         psnid = mysql.searchindb(user.id)
-                        say = '喔!'
-                whose = '的PSNID是: '
+                whose = '他的PSNID是：'
         except:
                 user = update.message.from_user
                 psnid = mysql.searchindb(user.id)
-                whose = '的PSNID是: '
-                say = '喔!'
+                whose = '你的PSNID是：'
         if(psnid == -1):
                 psnid = '沒有登錄'
         msgid = update.message.message_id
-        replyMsg(bot, update, str(whose) + str(psnid) + str(say))
+        replyMsg(bot, update, str(whose) + str(psnid))
 #        delmsg(bot, update)
         _thread.start_new_thread(delmsg,(bot, update) )
 

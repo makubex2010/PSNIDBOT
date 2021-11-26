22f5d8cb6ff9721eb1c4369a9cf86399859646b3
KevinChen <67411136+makubex2010@users.noreply.github.com>
2021-11-27 03:57:57 +0800

    Update psnidbot.py

--- psnidbot.py
+++ psnidbot.py
@@ -20,7 +20,7 @@ def sendMsg(bot, update, msg):
     bot.sendMessage(chat_id=update.message.chat_id, text = msg)
 
 def delmsg(bot, update):
-    time.sleep(300)
+    time.sleep(60)
     try:
         bot.delete_message(update.message.chat_id, update.message.message_id + 1)
     except:

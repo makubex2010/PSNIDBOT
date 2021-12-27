984a375decca3fc51d6aa689cfeee2b96d50121e
KevinChen <67411136+makubex2010@users.noreply.github.com>
2021-12-27 20:41:49 +0800

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -3,7 +3,7 @@
 import pymysql
 
 def connectDB(cmd): 
-        db = pymysql.connect(host='containers-us-west-21.railway.app', port='7012', user='root', passwd='eQYm2HwqtWARweRi5g6a', db='railway', charset='utf8')
+        db = pymysql.connect("jtb9ia3h1pgevwb1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "rfrwe8ksy97efx1k", "epawk56f10zxjf9b", "oyszn9xf2whgmb2j", charset='utf8')
         cursor = db.cursor()
         cursor.execute(cmd)
         data = cursor.fetchone()

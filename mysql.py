54f3990dc127edf80631c77b7a08247df810fc7a
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-03-07 00:11:26 +0800

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -3,7 +3,7 @@
 import pymysql
 
 def connectDB(cmd): 
-        db = pymysql.connect("containers-us-west-7.railway.app:7980", "root", "F3ymGOtjm2lryzTzjeAx", "railway", charset='utf8')
+        db = pymysql.connect("containers-us-west-7.railway.app", "root", "F3ymGOtjm2lryzTzjeAx", "railway", charset='utf8')
         cursor = db.cursor()
         cursor.execute(cmd)
         data = cursor.fetchone()
